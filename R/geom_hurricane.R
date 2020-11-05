b7b4a95182c1eb8e25c9fe2b5387d103fa6cc506
rsizem2 <sizemore.robert.k@gmail.com>
2020-11-05 16:42:30 -0500

    cleaned up documentation

--- R/geom_hurricane.R
+++ R/geom_hurricane.R
@@ -1,5 +1,6 @@
 #' Defines the GeomHurricane class via the ggproto function in ggplot2.
 #'
+#' The workhorse of this geom is the destPoint function from the `geosphere` package which takes a center point and radius and generates the vertices from which we can draw the polygonGrob. See \link{geom_hurricane} for specific details about parameters.
 #'
 #' @importFrom ggplot2 ggproto aes Geom draw_key_polygon
 #' @importFrom dplyr mutate select tibble rename
@@ -16,42 +17,38 @@ GeomHurricane <-
                      default_aes = ggplot2::aes(colour = "grey20",
                                                 fill = "grey20",
                                                 linetype = 0,
-                                                alpha = 0.65,
+                                                alpha = 0.6,
                                                 scale_radii= 1.0),
 
                      draw_key = ggplot2::draw_key_polygon,
 
                      draw_group = function(data, panel_scales, coord) {
+
+                         # Generate auxiliary data to be used with destPoint or polygonGrob
                          data <- dplyr::mutate(data, fill = fill, colour = colour)
                          centers <- dplyr::select(data, lon = x, lat = y)
                          radii <- dplyr::select(data, r_ne, r_se, r_sw, r_nw)
                          points <- dplyr::tibble()
 
-                         # iterate over each quadrant
+                         # Create circular quadrants using destPoint
                          for (i in 1:4){
-                             # iterate over rows
                              for (j in seq_len(nrow(data))){
                                  center <- dplyr::tibble(lon = centers[j,1], lat = centers[j,2])
-                                 points <- geosphere::destPoint(p = centers,
-                                                                b = ((i-1)*90):(90*i),
-                                                                d = data$scale_radii * 1852 * radii[j,i]) %>%
-                                     rbind(center, points)
+                                 arc_points <- geosphere::destPoint(p = centers,
+                                                                    b = ((i-1)*90):(90*i),
+                                                                    d = data$scale_radii * 1852 * radii[j,i])
+                                 points <- rbind(arc_points, center, points)
                              }
-
-                             # # Data Manipulation
-                             # vertices <- points %>%
-                             #     dplyr::rename(x = lon, y = lat) %>%
-                             #     coord$transform(panel_scales)
                          }
 
-                         # Data Manipulation
-                         vertices <- points %>%
+                         # Transform the data
+                         coords <- points %>%
                              dplyr::rename(x = lon, y = lat) %>%
                              coord$transform(panel_scales)
 
-                         # Plot the polygon
-                         grid::polygonGrob(x = vertices$x,
-                                           y = vertices$y,
+                         # Construct the polygonGrob
+                         grid::polygonGrob(x = coords$x,
+                                           y = coords$y,
                                            default.units = "native",
                                            gp = grid::gpar(col = data$colour,
                                                            fill = data$fill,
@@ -63,10 +60,21 @@ GeomHurricane <-
 
 #' Function that builds a layer based on the specification of the GeomHurricane class.
 #'
-#' @param data A dataframe containing hurricane data. Should include latitude and longitude
-#' data for the center of the hurricane as well as radii values indicating how far from the center
-#' of the hurricane certain windspeeds extend in the NE, NW, SW and SE directions.
+#' @param data A dataframe containing hurricane data. See details for expected formatting.
 #'
+#' @details Required aesthetics:
+#'   * x - longtitude of center.
+#'   * y - latitude of center.
+#'   * r_ne - radius from center point for northeast quadrant.
+#'   * r_se - radius from center point for southeast quadrant.
+#'   * r_nw - radius from center point for northwest quadrant.
+#'   * r_sw - radius from center point for southwest quadrant.
+#'   Optional aesthetics:
+#'   * alpha - transparency of the drawn layer, default = 0.6.
+#'   * fill - fill color, default = 'grey20'.
+#'   * color - outline color, default = 'grey20'.
+#'   * scale_radius - scaling factor for radius, default = 1.0 (no scaling).
+#' @md
 #'
 #' @examples
 #'   \dontrun{ggplot(data = katrina) +
